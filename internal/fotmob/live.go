d5b484f8b3f413bd4dbd4b970b688f7d0a894300
Jose Contreras (Potter) <jose.contreras.godoy@gmail.com>
2026-02-12 21:18:44 -0600

    Small fix for first fetch of live matches

--- internal/fotmob/live.go
+++ internal/fotmob/live.go
@@ -67,8 +67,10 @@ func (c *Client) LiveMatchesForLeague(ctx context.Context, leagueID int) ([]api.
 	for _, match := range matches {
 		// Verify match is for today and is live
 		if match.MatchTime != nil {
-			matchDate := match.MatchTime.UTC().Format("2006-01-02")
+			//Compare with local times
+			matchDate := match.MatchTime.Local().Format("2006-01-02")
 			if matchDate == dateStr && match.Status == api.MatchStatusLive {
+				fmt.Printf("live.Status is %s\n",match.Status)
 				liveMatches = append(liveMatches, match)
 			}
 		}
